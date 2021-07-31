a32f25de8b21c7e0c780c29dcc7aa55595b4c69f
Gurpreet <66855895+gudhillon@users.noreply.github.com>
2021-07-31 15:46:15 -0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+// Webpack Config, leads to efficiency in development
+
 module.exports = merge(common, {
   mode: 'development',
 });
