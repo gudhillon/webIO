8040a54e1faeb04d313121a70688d4ea06719044
Gurpreet <66855895+gudhillon@users.noreply.github.com>
2021-07-31 15:45:17 -0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+// Optimize bundle sizes when transitioning to production
+
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
